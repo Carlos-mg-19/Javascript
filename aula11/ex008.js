405b757acf0813972faf1bfcb7cee41e39b6559c
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2022-12-21 20:37:06 -0300

    commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocida do seu carro ${vel} km/h`)
+if ( vel > 60) {
+    console.log(`voce sera multado assima da velocidade permitida`)
+}
+console.log(`dirija sempre usando sinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+    console.log(`vivendo ${pais}`)
+if (pais == 'brasil') {
+    console.log(`Eu sou brasileiro`)
+}else{
+    console.log(`Não sou brasileiro`)
+}
