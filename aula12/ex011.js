538442bf7f2bd7b6fdd2b8dfe5ad369513da0269
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2022-12-26 19:01:50 -0300

    commit

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 65
+if (idade < 16) {
+    console.log(`não volta`)
+} else if (idade < 18 || idade >= 65) {
+    console.log(`voto opicional`)
+}else{
+    console.log(`voto obrigartorio`)
+}
