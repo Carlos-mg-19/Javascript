9a487381e314e2a41d3f67c7265a981481ee64af
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2022-12-30 22:47:20 -0300

    commit

--- aula12.ex2/modelo/script.js
+++ aula12.ex2/modelo/script.js
@@ -3,42 +3,45 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    var img = document.getElementById('image')
+    
     
     
     if (fano.value.length == 0 || Number(fano.value) > ano){
-        window.alert('erro')
+        window.alert('[ERRO] ')
+    
     }else{
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var foto = document.getElementById('image')
         
         
         if (fsex[0].checked){
+            
             genero = 'Homen'
             if (idade >=0 && idade < 12){
-            
+                foto.src = 'imagens/Criança.M.png'
                 
-        
             }else if (idade < 25){
-                img.src = 'imagens/18.M.png'
+                foto.src = 'imagens/18.M.png'
             }else if (idade < 45){
-
+                foto.src = 'imagens/45.M.png'
             }else {
-
+                foto.src = 'imagens/65.M.png'
             }
         }else if (fsex[1].checked){
             genero = 'Mulher'
             if (idade >=0 && idade < 12){
-                
+                foto.src = 'imagens/Criança.F.png'
             }else if (idade < 25){
-                
+                foto.src = 'imagens/18.F.png'
             }else if (idade < 45){
-
+                foto.src = 'imagens/45.F.png'
             }else {
-                
+                foto.src = 'imagens/65.F.png'
             }
         }
+        
         res.innerHTML = `detectamos um ${genero} com ${idade} anos.`
     }
 }
