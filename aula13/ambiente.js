994ba0e14eac5639d72e879174c0ad52434408f3
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2023-01-03 10:44:30 -0300

    commit

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -2,11 +2,11 @@ var c = 1
 do { 
     console.log(`${c} tudo bem`)
     c++
-}while (c <= 10)
+}while (c <= 10)  /*testes logico e feito no final */
 
 /* 
 var c = 1
 while (c <= 10){
     console.log(`${c} tudo bem`)
-}
+} teste logico e feito no inicio
 */
---
+++ aula14.ex/ex015/script.js
@@ -0,0 +1,13 @@
+function contar(){
+    var nume1 = document.getElementById('num1')
+    var nume2 = document.getElementById('num2')
+    var nume3 = document.getElementById('num3')
+    var n1 = Number(nume1.value)
+    var n2 = Number(nume2.value)
+    var n3 = Number(nume3.value)
+    var res = document.getElementById('res')
+
+    for (var c = n1; c < n2; c++){
+        res.innerHTML = `.${c},`
+    }
+}
