8d47f00f9073b47d3765d0a93807713924923e96
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2023-01-03 14:23:29 -0300

    Update script.js

--- aula14.ex/ex015/script.js
+++ aula14.ex/ex015/script.js
@@ -1,13 +1,32 @@
 function contar(){
-    var nume1 = document.getElementById('num1')
-    var nume2 = document.getElementById('num2')
-    var nume3 = document.getElementById('num3')
-    var n1 = Number(nume1.value)
-    var n2 = Number(nume2.value)
-    var n3 = Number(nume3.value)
-    var res = document.getElementById('res')
+    let ini = document.getElementById('num1')
+    let fim = document.getElementById('num2')
+    let pass = document.getElementById('num3')
+    let res = document.getElementById('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0){
+        window.alert('ERRO FALTA DADOS')
+    }else{
+         res.innerHTML = 'Contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pass.value)
 
-    for (var c = n1; c < n2; c += n3){
-        res.innerHTML += `<div>${c},</div>`
+        if(p <= 0){
+            window.alert('passo sera trocado para 1')
+            p = 1
+        }
+        if (i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} `
+            }
+        }
+        if(i > f){
+            for(let c = i ; c >= f; c -= p){
+                res.innerHTML += ` ${c} `
+            }
+        }
+    
     }
+        
+    
 }
