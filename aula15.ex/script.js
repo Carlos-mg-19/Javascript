393b928d5a39135e6443ec63bad4e1e34f02a1e4
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2023-01-12 20:07:39 -0300

    commit

--- aula15.ex/script.js
+++ aula15.ex/script.js
@@ -1,7 +1,28 @@
-function adicionar(){
-    let num = document.getElementById('numero')
+let num = document.getElementById('txnum')
+let sel = document.getElementById('select')
+let res = document.getElementById('res')
+let valores = []
 
-    if (num.value.length == 0){
-        window.alert('digite um numero para adionar')
+function isnumeros(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{ 
+        return false
+    }
+}
+
+function inlista(n, 1){
+    if(1.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adionar(){
+    if(isnumeros(num.value) && !inlista(num.value, valores)){
+        window.alert('tudo ok')
+    }else{
+        window.alert('valores invalido ja encontrado na lista')
     }
 }
