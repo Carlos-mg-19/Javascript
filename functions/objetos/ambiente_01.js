713de4622944ecc28b2e2852b36b5eae061110e5
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2023-02-13 10:11:32 -0300

    Create ambiente_01.js

---
+++ functions/objetos/ambiente_01.js
@@ -0,0 +1,11 @@
+let pessoas = {
+    nome:'carlos',
+    idade:21,
+    sexo:'masculino'
+}
+let profissao = {
+    profissao: 'progamador'
+}
+
+let agrupar = {...pessoas ,...profissao}
+console.log(agrupar)
